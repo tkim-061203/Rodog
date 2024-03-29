7cc7aba1199a5db12d778986b05269ecfda26646
tkim-061203 <nntkim.work@gmail.com>
2024-03-29 22:50:57 +0700

    first upload

---
+++ build/rodog_description/CMakeFiles/rodog_description_uninstall.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty custom commands generated dependencies file for rodog_description_uninstall.
+# This may be replaced when dependencies are built.
---
+++ build/rodog_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kim/rodog_ws/src/rodog_description
+# Build directory: /home/kim/rodog_ws/build/rodog_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
